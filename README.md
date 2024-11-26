dffc1d5e8da5b35154a6c43f2aeca6db8ecce294
Florian Coussy <florian.coussy@gmail.com>
2024-11-26 17:18:20 -0500

    Add GET tours endpoint

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # MERN Stack
 
 Master Node by building a real-world RESTful API and web app (with authentication, Node.js security, payments &amp; more)
+
+### Stack
+
+- Express, NodeJS, JavaScript
+- Prettier
+- VSCode
--- app.js
+++ app.js
@@ -1,11 +1,24 @@
+const fs = require("fs");
 const express = require("express");
 
+const API_VERSION = "/api/v1";
 const PORT = 3000;
+const FILE_FORMAT = "UTF-8";
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.status(200).send("Hello from the server side!");
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/data/tours-simple.json`, FILE_FORMAT)
+);
+
+app.get(`${API_VERSION}/tours`, (req, res) => {
+  res.status(200).json({
+    status: "success",
+    results: tours.length,
+    data: {
+      tours,
+    },
+  });
 });
 
 app.listen(PORT, () => {
