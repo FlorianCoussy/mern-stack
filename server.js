eac8fd039ff702fb27efd9a51b018639e4e96b21
Florian Coussy <florian.coussy@gmail.com>
2024-12-18 18:58:19 +0100

    Connect the server to MongoDB local database

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 const dotenv = require("dotenv");
+const mongoose = require("mongoose");
 
 // Load the .env config file
 dotenv.config({ path: `${__dirname}/config.env` });
 
 const app = require("./app");
+
+mongoose
+  .connect(process.env.DATABASE_URI)
+  .then((db) =>
+    console.log(`Connection to ${db.connections[0].name} DB successful!`)
+  );
 
 app.listen(process.env.PORT, () => {
   console.log(`App running on port ${process.env.PORT}...`);
