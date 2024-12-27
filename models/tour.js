a9041acf12077f37b968192ae633d47abf234863
Florian Coussy <florian.coussy@gmail.com>
2024-12-27 12:42:52 +0100

    Update the Tour Schema with more data

--- models/tour.js
+++ models/tour.js
@@ -6,17 +6,52 @@ const tourSchema = new mongoose.Schema({
     type: String,
     required: [true, "A tour must have a name"], // Validator
     unique: true,
+    trim: true,
   },
-  rating: {
+  summary: {
+    type: String,
+    required: [true, "A tour must have a summary"],
+    trim: true,
+  },
+  description: {
+    type: String,
+    trim: true,
+  },
+  imageCover: {
+    type: String,
+    required: [true, "A tour must have a cover image"],
+  },
+  images: [String],
+  duration: {
+    type: Number,
+    required: [true, "A tour must have a duration"],
+  },
+  difficulty: {
+    type: String,
+    required: [true, "A tour must have a difficulty"],
+  },
+  maxGroupSize: {
+    type: Number,
+    required: [true, "A tour must have a maximum group size"],
+  },
+  startDates: [Date],
+  ratingsAverage: {
     type: Number,
     default: 4.5,
+  },
+  ratingsQuantity: {
+    type: Number,
+    default: 0,
   },
   price: {
     type: Number,
     required: [true, "A tour must have a price"],
   },
-  difficulty: String,
-  premium: Boolean,
+  discount: Number,
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
 });
 
 // A Model acts as a wrapper for the schema providing interface for CRUD operations
